72f9b4aef4aec6f8c5172e836e91be5ca3e721f9
djolez <djolez@gmail.com>
2017-10-21 14:00:57 +0200

    Uncommented lines related to serial

--- server/comm_implementation.py
+++ server/comm_implementation.py
@@ -6,13 +6,13 @@ import logging
 import config
 import global_handler as gh
 
-#serial_conn = serial.Serial(config.SERIAL_PORT, config.SERIAL_BAUD_RATE, write_timeout = 1)
+serial_conn = serial.Serial(config.SERIAL_PORT, config.SERIAL_BAUD_RATE, write_timeout = 1)
 logger = logging.getLogger(__name__)
 
 def send_msg(msg):
     try:
         logger.debug("Sending message '{}'".format(msg))
-        #serial_conn.write(bytearray(msg + '\n', encoding='utf-8'))
+        serial_conn.write(bytearray(msg + '\n', encoding='utf-8'))
     except serial.SerialTimeoutException as e:
         pass
         #logger.exception("Serial timeout expired")
@@ -25,13 +25,14 @@ def main():
 
     while True:
         try:
-            pass
-            #raw = serial_conn.readline()
-            #data = str(raw, 'utf-8')
-            #handle_msg(data)
+            #pass
+            print("HERE")
+            raw = serial_conn.readline()
+            data = str(raw, 'utf-8')
+            handle_msg(data)
         except TypeError as e:
             logging.exception(e)
         except OSError as e:
             logging.exception(e)
 
-#threading.Thread(target=main).start()
+threading.Thread(target=main).start()
